e1c75a01d450672872f5ec108616b005e3b0ffc7
Douglas A.C <dac17@inf.ufpr.br>
2018-10-06 19:04:36 -0300

    Limpando.

--- ConexaoRawSocket.h
+++ ConexaoRawSocket.h
@@ -1,51 +1,9 @@
-// adicionar IFDEF
+#ifndef _ConexaoRawSocket_
+#define _ConexaoRawSocket_
 
-int ConexaoRawSocket(char *device);
+    /**
+     * Retona file descriptor da conexão criada.
+    */
+    int ConexaoRawSocket(char *device);
 
-typedef struct Mensagem{
-    
-    unsigned char marcador_inicio;
-    unsigned char tamanho;
-    unsigned char sequencia;
-    unsigned char tipo;
-    char dados;
-    unsigned char crc;
-    
-}Mensagem;
-
-typedef struct MensagemContent{
-    int a;
-    int b;
-    int c;
-    int d;
-    int e;
-    int f;
-    int g;
-}MensagemContent;
-
-enum Tipos{
-    NACK,
-    ACK,
-    OK,
-    DADOS,
-    NONE1,
-    FD,  // file descriptor
-    NONE2,
-    SHOW_SCREEN,
-    NONE3,
-    NONE4,
-    GET,
-    PUT,
-    CD,
-    LS,
-    ERRO,
-    FIM
-};
-
-enum Erros{
-    NONE6,
-    INEXISTENTE,
-    PERMICAO,
-    QUOTA,
-    NONE7
-};
+#endif
