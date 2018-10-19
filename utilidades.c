f6afd973f9adfa39ac93d15c1224931764e2b18b
rafaelatc3sl <rpd17@c3sl>
2018-10-19 13:19:06 -0300

    gottaslepp
    Signed-off-by: rafaelatc3sl <rpd17@c3sl>

--- utilidades.c
+++ utilidades.c
@@ -28,3 +28,77 @@ void recuperaMensagem(Mensagem * msg, void * buffer){
     memcpy(&(msg->crc),buffer + 3 + msg->controle.tamanho, 1);    
     
 }
+
+
+
+/*
+    Inicialização:
+        - /
+        String acumula a posição atual
+*/
+
+/*
+    Todas devem possuir
+
+    imprimir o local atual acessado por cd/ls
+    Estados:
+        - $ local
+        - $ remoto
+
+            
+*/
+
+/*
+    Comentarios Rafael noob : olhar onde o programa esta sendo executado
+
+*/
+void local_cd(){
+    // verificar permissão/existência
+    // int access(const char *, int); retorno errno
+    // printar na tela o ERRO possivel
+}
+
+void local_ls(){
+    // não precisa verificar autorização
+    // utilizar ls do sistema
+    // folder atual
+    // nosso pwd relativo
+}
+
+void remote_cd(){
+    // verificar permissão/existência
+    // int access(const char *, int); retorno errno
+    // devolver como mensagem slave > master
+}
+
+void remote_ls(){
+    // não precisa verificar autorização
+    // devolver como mensagem slave > master
+
+}
+
+void put(){
+    
+    // envia o nome
+    // abre write com esse nome
+    // espera pela resposta OK
+    // enviar descritor/tamanho do arquivo / verificar espaço
+    // espera resposta ok ou erro se erro olhar dados
+    // começa enviar dados 
+    // le 127 em 127 adiciona em uma mensagem incrementa , tam/127
+    // if < 127 
+    // getchar ++ até construir mensagem , deu 127 faz não deu trata 
+}
+
+void get(){
+    // envia o nome
+    // abre write com esse nome
+    // espera pela resposta OK
+    // enviar descritor/tamanho do arquivo / verificar espaço
+    // espera resposta ok ou erro se erro olhar dados
+    // começa enviar dados 
+    // le 127 em 127 adiciona em uma mensagem incrementa , tam/127
+    // if < 127 
+    // getchar ++ até construir mensagem , deu 127 faz não deu trata 
+}
+
