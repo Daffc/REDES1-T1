e2a86ec4748c3af22cd859cab9ca6aa10e785ed5
Douglas A.C <dac17@inf.ufpr.br>
2018-10-20 20:01:02 -0300

    Definindo strings de localização e verificando comandos.

--- master.c
+++ master.c
@@ -10,50 +10,82 @@
 
 int main(){
 
-    int         filedesk;
-    char        retorno[1035], comando[500];
+    int         conexao;
+    char        retorno, comando[100], local[500], remoto[500];
     void        *buffer;
-    FILE        *fpls;
 
-    filedesk = ConexaoRawSocket("lo");
+    conexao = ConexaoRawSocket("lo");
 
-    buffer = malloc(300);
+    // buffer = malloc(300);
     
     Mensagem    msg;
     msg.dados = malloc(127);
         
 
-    msg.marcador_inicio = 126;
-    msg.controle.tamanho = 127;
-    msg.controle.sequencia = 15;
-    msg.controle.tipo = 1;
-    strcpy(msg.dados, "Surprise steepest recurred landlord mr wandered amounted of. Continuing devonshire but considered its. Rose past oh shew roof is song neat. Do depend better");
-    msg.crc = 81;
+    strcpy(local, "~/");
+    strcpy(remoto, "~/");
 
-    defineBuffer(&msg, buffer);
+    // msg.marcador_inicio = 126;
+    // msg.controle.tamanho = 127;
+    // msg.controle.sequencia = 15;
+    // msg.controle.tipo = 1;
+    // msg.crc = 81;
+
+    // defineBuffer(&msg, buffer);
     
-    //NÃO ENVIA MSG SE TAMANHO DA MENSAGEM FOR MENOR QUE 14(BYTES)
-    int resp = send(filedesk, buffer, 4 + msg.controle.tamanho, 0);
-    printf("Verificação envio: %d\n", resp);
-    
+    // // NÃO ENVIA MSG SE TAMANHO DA MENSAGEM FOR MENOR QUE 14(BYTES)
+    // int resp = write(conexao, buffer, 4 + msg.controle.tamanho);
+    // printf("Verificação envio: %d\n", resp);
 
     // TESTES COM COMANDOS !!!
-    // while(1){
-    //     // Le linha de comando indicada por usuario.
-    //     scanf(" %99[^\n]", comando);
+    while(1){
+        imprimeLocalizacao(local, remoto);
 
-    //     // Inicializa descritor com resposta do comando indicado.
-    //     fpls = IniciaDescritorComando(comando);
+        // Le linha de comando indicada por usuario.
+        scanf(" %99[^\n]", comando);
 
-    //         /*Le linha por linha arquivo de resposta do ls aberto e gerencia tratamento*/
-    //         while (fgets(retorno, 127, fpls) != NULL) {
-    //             printf("%s|", retorno);
-    //         }
+        /**
+         * Caso comando inicie com a String "ls".
+        */
+        if(strstr(comando, "ls") ==  comando){
+            local_ls(comando, local);
+        } 
 
-    //     // Finaliza descritor utilizado.
-    //     FinalizaDescritorComando(fpls);
-    // }    
+        /**
+         * Caso comando inicie com a String "ls".
+        */
+        if(strstr(comando, "cd") ==  comando){
+            printf("COMANDO CD\n");
+        } 
 
+        /**
+         * Caso comando inicie com a String "ls".
+        */
+        if(strstr(comando, "rls") ==  comando){
+            printf("COMANDO RLS\n");
+        } 
+
+        /**
+         * Caso comando inicie com a String "ls".
+        */
+        if(strstr(comando, "rcd") ==  comando){
+            printf("COMANDO RCD\n");
+        } 
+
+        /**
+         * Caso comando inicie com a String "ls".
+        */
+        if(strstr(comando, "get") ==  comando){
+            printf("COMANDO GET\n");
+        } 
+
+        /**
+         * Caso comando inicie com a String "ls".
+        */
+        if(strstr(comando, "put") ==  comando){
+            printf("COMANDO PUT\n");
+        } 
+    }    
     
     return 0;
 }
